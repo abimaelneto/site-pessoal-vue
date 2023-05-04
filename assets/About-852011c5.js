821e5ba311ebbd1f4353dce4c814f111986b5308
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-05-04 11:32:59 -0300

    deploy

---
+++ assets/About-852011c5.js
@@ -0,0 +1,1 @@
+import{_ as o,o as e,c as t}from"./index-9dea8adf.js";const a={};function n(c,r,s,p,_,u){return e(),t("div",null,"minha nova about page")}const i=o(a,[["render",n]]);export{i as default};
--- assets/About-e6c15978.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as e,c as t}from"./index-db83562e.js";const a={};function n(c,r,s,p,_,u){return e(),t("div",null,"minha nova about page")}const i=o(a,[["render",n]]);export{i as default};
