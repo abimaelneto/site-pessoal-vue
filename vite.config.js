530f2244f5486af104c669c1c3147a4ba44407b8
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-05-04 11:37:14 -0300

    adiciona base no vite config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  publicPath: process.env.NODE_ENV === 'production'
+  base: process.env.NODE_ENV === 'production'
     ? '/site-pessoal-vue/'
     : '/'
 })
